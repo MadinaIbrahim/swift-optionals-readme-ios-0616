5c61c608083f9c9738155bf7e3f8195273b78cb0
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-07-13 21:02:05 -0400

    Done.

--- Pets.playground/Contents.swift
+++ Pets.playground/Contents.swift
@@ -1,18 +1,23 @@
+
 var petName: String?
+
 petName = "Scooter 🐢"
 print(petName)
-
+//checking if a value in an Optional exists by testing if it is equal to nil or not.
 if petName != nil {
     print("My friend's pet is \(petName)")
 } else {
     print("My friend doesn't have a pet 😞")
 }
 
+///To unwrap an Optional, you use a Swift feature called optional binding.
 if let petName = petName {
     print("My friend's pet is \(petName)")
+    
+    print("My pet is \(petName)")
 }
 
 if let friendsPet = petName {
     print("My friend's pet is \(friendsPet)")
-    print("petName is still an Optional! \(petName)")
+  
 }
